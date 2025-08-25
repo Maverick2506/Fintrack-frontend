91fb7546dfb550bc1d8b34367001af69e8de7570
Maverick2506 <sisodi.aditya83@gmail.com>
2025-08-25 15:03:59 -0400

    Updated all the urls

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 // Login function no longer needs an email parameter
 export const login = async (password) => {
